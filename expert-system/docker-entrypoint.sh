cfc764a3c3a9ff26c3ddbe859e9752d5cff2d98d
nikitosss <nikitka.patrakov@mail.ru>
2023-09-17 13:40:52 +0300

    refactoring

---
+++ expert-system/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+poetry run task makemigrations
+poetry run task migrate
+poetry run task createsuperuser --noinput
+poetry run task start 0:8000
+
+exec "$@"
