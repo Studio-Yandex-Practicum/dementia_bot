9a20a1ae06d616dd64dcd5b8efcd3a265fa6e498
VladPetukhin <joypetshop@yandex.ru>
2023-09-18 19:32:14 -0300

    добавил урлы

--- expert-system/botapp/serializers.py
+++ expert-system/botapp/serializers.py
@@ -4,14 +4,15 @@ from .models import Test, Question
 
 class QuestionSerializer(serializers.ModelSerializer):
     """Сериализатор для вопросов. Переопределил поля как в задании."""
-    
+
     questionId = serializers.IntegerField(source='id')
     type = serializers.CharField(source='question_type')
     question = serializers.CharField(source='text')
 
     class Meta:
         model = Question
-        fields = ('questionId', 'type', 'question') 
+        fields = ('questionId', 'type', 'question')
+
 
 class TestSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True)
--- expert-system/botapp/views.py
+++ expert-system/botapp/views.py
@@ -8,12 +8,17 @@ from rest_framework import status
 @api_view(['GET'])
 def get_test(request, test_id):
     """Получаем айди теста и возвращаем впоросы к нему"""
-    
+
     try:
         test = Test.objects.get(id=test_id)
         serializer = TestSerializer(test)
         return Response(serializer.data)
-    
-    except Test.DoesNotExist:
-        return Response({"error": "Test not found"}, status=404)
 
+    except Test.DoesNotExist:
+        return Response({"error": "Test not found"},
+                        status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def submit_test(request):
+    pass
