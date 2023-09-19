6c0fa4e7b5605e2e2a2f4e556335a5646e1eee4a
VladPetukhin <joypetshop@yandex.ru>
2023-09-19 01:11:27 -0300

    сделал пост запросы, вьюху, сериализатор, валидатор

--- expert-system/botapp/urls.py
+++ expert-system/botapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import get_test, submit_test
 
 urlpatterns = [
--- expert-system/botapp/views.py
+++ expert-system/botapp/views.py
@@ -1,8 +1,10 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .models import Test
-from .serializers import TestSerializer
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Test
+from .serializers import TestDataSerializer, TestSerializer
+from .utils import create_participant, create_user_answers
 
 
 @api_view(['GET'])
@@ -15,10 +17,22 @@ def get_test(request, test_id):
         return Response(serializer.data)
 
     except Test.DoesNotExist:
-        return Response({"error": "Test not found"},
+        return Response({"error": "Тест не найден"},
                         status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST'])
 def submit_test(request):
-    pass
+    """Получаем данные от пользователя и сохраняем их в БД."""
+
+    serializer = TestDataSerializer(data=request.data)
+
+    if not serializer.is_valid():
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    validated_data = serializer.validated_data
+    participant = create_participant(validated_data)
+    create_user_answers(participant, validated_data)
+
+    return Response({"message": "Тест завершен успешно!"},
+                    status=status.HTTP_201_CREATED)
