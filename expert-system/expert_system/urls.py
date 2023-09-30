669639074bde10e571f20f6c15c1d8f979a4b60e
VladPetukhin <joypetshop@yandex.ru>
2023-09-29 21:48:56 -0300

    добавил сериалайзер, изменил модели, добавил админку и добавил сваггер для работы с апи

--- expert-system/expert_system/urls.py
+++ expert-system/expert_system/urls.py
@@ -1,7 +1,29 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Бот деменция",
+        description="API для бота, проверяем ответ сервера",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.IsAdminUser,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
     path('', include('botapp.urls')),
 ]
+
+# Только при разработке, на продакшене убираем
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
