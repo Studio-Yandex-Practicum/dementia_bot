64c5385757d45eff6a34a723504d28288705a730
Alena Meshkova <alenka.meshkova@yandex.ru>
2023-10-25 23:31:52 +0300

    отработка api/submit

--- expert-system/botapp/urls.py
+++ expert-system/botapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import get_all_tests, get_test, submit_test, submit_result, get_result
+from .views import get_all_tests, get_test, submit_test,  get_result
 
 urlpatterns = [
     path('api/tests/', get_all_tests, name='all_test'),
     path('api/test/<int:test_id>/', get_test, name='get_test'),
     path('api/submit/', submit_test, name='submit'),
-    path('api/submit_result/', submit_result, name='submit_result'),
     path('api/get_result/<int:telegram_id>/', get_result, name='get_result'),
 ]
