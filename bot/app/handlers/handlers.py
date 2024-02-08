8a7a9825c442b74647bf807eb48e8eec6a5f163a
Vladimir <vdnikulin24@gmail.com>
2024-02-08 22:12:51 +0500

    Добавил новые команды

--- bot/app/handlers/handlers.py
+++ bot/app/handlers/handlers.py
@@ -11,13 +11,36 @@ async def command_start(message: types.Message, bot: Bot):
 
     menu_commands = [
         BotCommand("/start", "Начать"),
-        BotCommand("/help", "Помощь")
+        BotCommand("/help", "Помощь"),
+        BotCommand("/test", "Пройти тест"),
+        BotCommand("/signup", "Записаться в центр"),
+        BotCommand("/participate", "Участвовать в мероприятиях"),
     ],
 
     await bot.set_chat_menu_button(
         chat_id=message.chat.id,
         menu_button=MenuButtonCommands(commands=menu_commands)
     )
+
+    message_text = """
+    Проект Деменция.net создан в  2021 году благотворительным фондом «Память поколений».
+
+    Благотворительный фонд «Память поколений» был основан  22 июня  2015 года – в День памяти и скорби.
+
+    Наш фонд помогает ветеранам Великой Отечественной войны и современных боевых действий (в Афганистане, Чечне, Сирии). Всего за время существования фонда мы помогли более  18000 ветеранам.
+
+    Это огромное множество операций, курсов реабилитации, современных протезов и слуховых аппаратов, дорогостоящих колясок и комплектов медикаментов, средств личной гигиены. Но, что важнее, это тысячи изменившихся к лучшему жизней людей.
+
+    Вы можете пройти тесты для проверки себя или близкого.
+    """
+
+    await message.answer(
+        f"Здравствуйте, {hbold(message.from_user.full_name)}, {message_text}!"
+    )
+
+
+@my_router.message(Command("test"))
+async def command_test(message: types.Message):
 
     inline_kb = types.InlineKeyboardMarkup()
 
@@ -31,20 +54,9 @@ async def command_start(message: types.Message, bot: Bot):
             web_app=types.WebAppInfo(url="https://testing2.xn--d1acamsh7dwd.net/testrelUser")
         )
     )
-    message_text = """\
-    Проект Деменция.net создан в  2021 году благотворительным фондом «Память поколений».
-
-    Благотворительный фонд «Память поколений» был основан  22 июня  2015 года – в День памяти и скорби.
-
-    Наш фонд помогает ветеранам Великой Отечественной войны и современных боевых действий (в Афганистане, Чечне, Сирии). Всего за время существования фонда мы помогли более  18000 ветеранам.
-
-    Это огромное множество операций, курсов реабилитации, современных протезов и слуховых аппаратов, дорогостоящих колясок и комплектов медикаментов, средств личной гигиены. Но, что важнее, это тысячи изменившихся к лучшему жизней людей.
-
-    Вы можете пройти тесты для проверки себя или близкого.\
-    """
 
     await message.answer(
-        f"Здравствуйте, {hbold(message.from_user.full_name)}, {message_text}!",
+        "Вы можете выбрать один из тестов для прохождения: Проверь себя или Проверь близкого!",
         reply_markup=inline_kb
     )
 
@@ -52,4 +64,43 @@ async def command_start(message: types.Message, bot: Bot):
 @my_router.message(Command("help"))
 async def command_help(message: types.Message):
 
-    await message.answer("С помощью команды /start вы можете запустить бота и пройти тесты!")
+    await message.answer("""
+    С помощью команды /start вы можете запустить бота и пройти тесты!
+
+    С помошью команды /test вы можете пройти тесты Проверь себя и Проверь близкого!
+
+    С помощью команды /signup вы можете записаться в центр!
+
+    С помощью команды /participate вы можете принять участие в мероприятиях!
+
+    """)
+
+
+@my_router.message(Command("/signup"))
+async def command_signup(message: types.Message):
+
+    inline_kb = types.InlineKeyboardMarkup()
+
+    inline_kb.row(
+        types.InlineKeyboardButton(
+            text="Записаться в центр",
+            web_app=types.WebAppInfo(url="https://sber.xn--d1acamsh7dwd.net/")
+        )
+    )
+
+    await message.answer("Нажмите на кнопку, чтобы записаться в центр!")
+
+
+@my_router.message(Command("/participate"))
+async def command_participate(message: types.Message):
+
+    inline_kb = types.InlineKeyboardMarkup()
+
+    inline_kb.row(
+        types.InlineKeyboardButton(
+            text="Участвовать в мероприятиях",
+            web_app=types.WebAppInfo(url="https://sber.деменция.net/")
+        )
+    )
+
+    await message.answer("Нажмите на кнопку, чтобы участвовать в мероприятиях!")
